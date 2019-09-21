74c1dc43477c809889ebb5e35d8a0a14081b4ae8
CindyChenx <chen_cx0128@hotmail.com>
2019-09-21 13:14:22 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artist1 = Artist.create!(name: "Beyonce")
+artist1.songs.create!(title: "10 Ways You Are Already Awesome")
+artist1.songs.create!(title: "This Yoga Stretch Cures Procrastination, Maybe")
+artist1.songs.create!(title: "The Power of Positive Thinking and 100 Gallons of Coffee")
+ 
+artist2 = Artist.create!(name: "JayZ")
+artist2.songs.create!(title: "Top 20 Summer Blockbusters Featuring a Cute Dog") 
